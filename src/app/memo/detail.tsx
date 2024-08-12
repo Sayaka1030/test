6461596aff955e4c8b57489161c84547915595f7
Sayaka1030 <a8122004@gsuite.si.aoyama.ac.jp>
2024-08-12 23:37:36 +0900

    79 headerをlayoutで作成、反映

--- src/app/memo/detail.tsx
+++ src/app/memo/detail.tsx
@@ -1,6 +1,5 @@
 import { View, Text, ScrollView, StyleSheet } from "react-native";
 
-import Header from "../../components/Header";
 import CircleButton from "../../components/CircleButton";
 import Icon from "../../components/Icon";
 import { router } from "expo-router";
@@ -12,7 +11,6 @@ const handlePress = (): void => {
 const Detail = ():JSX.Element =>{
     return (
         <View style={styles.container}>
-            <Header />
             <View style={styles.memoHeader}>
                 <Text style={styles.memoTitle}>買い物リスト</Text>
                 <Text style={styles.memoDate}>2023年10月1日 10:00</Text>
@@ -26,7 +24,7 @@ const Detail = ():JSX.Element =>{
                     </Text>
                 </ScrollView>
             </View>
-            <CircleButton onPress={handlePress} style={{ top: 160, bottom: 'auto' }}>
+            <CircleButton onPress={handlePress} style={{ top: 60, bottom: 'auto' }}>
                 <Icon name='pencil' size={40} color='#fff' />
             </CircleButton>
         </View>
